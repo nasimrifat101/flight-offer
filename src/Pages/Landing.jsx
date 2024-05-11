e76ab6994fdee4fa2bd7e0da7c26ab191f4760a8
nasimrifat101 <nasimrifat101@gmail.com>
2024-05-11 22:19:58 +0600

    [Master Price]: headline added

--- src/Pages/Landing.jsx
+++ src/Pages/Landing.jsx
@@ -3,7 +3,11 @@ import Navbar from "../Components/Navbar";
 const Landing = () => {
     return (
         <>
-            <Navbar/>
+            <Navbar />
+            <div className='max-w-screen-2xl mx-auto p-2'>
+                <h1 className="text-3xl font-bold">Master Price</h1>
+            </div>
+            <hr className="border-gray-300 border-1" />
         </>
     );
 };
